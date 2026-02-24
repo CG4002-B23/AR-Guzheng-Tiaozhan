43c76460f8c8d623166c74e07d017082efb62454
NicholasTanYY <nicholastanyunyu@gmail.com>
2026-02-24 10:42:34 +0800

    Add logic to toggle between various UI screens

--- Assets/Scripts/GameState/UIManager.cs
+++ Assets/Scripts/GameState/UIManager.cs
@@ -6,6 +6,47 @@ public class UIManager : MonoBehaviour
     [Tooltip("GameStateManager object holding the GameManager script")]
     public GameManager gameManager;
 
+    [Header("UI Canvases")]
+    [Tooltip("Drag Canvas panel objects here")]
+    public GameObject startMenu;
+    public GameObject pauseMenu;
+    public GameObject gameplayUI;
+
+    void OnEnable()
+    {
+        GameManager.OnGameStateChanged += HandleGameStateChange;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStateChanged -= HandleGameStateChange;
+    }
+
+    private void HandleGameStateChange(GameManager.GameState newState)
+    {
+        startMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        gameplayUI.SetActive(false);
+
+        switch (newState)
+        {
+            case GameManager.GameState.StartMenu: startMenu.SetActive(true);
+                break;
+                
+            case GameManager.GameState.Paused:
+                pauseMenu.SetActive(true);
+                gameplayUI.SetActive(true);  // set the gameplayUI to be visible behind the pause menu
+                break;
+                
+            case GameManager.GameState.Playing:
+            case GameManager.GameState.GuzhengPlacing:
+            case GameManager.GameState.PlayingFieldScanning:
+                // show the gameplayUI for all active game states
+                gameplayUI.SetActive(true);
+                break;
+        }
+    }
+
     // hook each of these functions to the OnClick() list of the buttons in the menus
     public void OnStartButtonClicked()
     {
